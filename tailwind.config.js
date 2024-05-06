cc75d678af1da2b4415d5d0e45d276eb226850b9
premalnayee <premnayee1@gmail.com>
2024-05-06 15:56:53 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: ['./dist/**/*.html'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    debugScreens: {
+      position: ['top', 'left'],
+    },
+    extend: {
+      fontFamily: {
+        headline: ['Oswald']
+       },
+       colors: {
+        mainColor: '#212f49'
+       } 
+    },
+  },
+  variants: {
+    extend: {},
+  },
+}
